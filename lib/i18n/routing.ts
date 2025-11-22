a533f3fcb733bbb548b221d84fef4ead3f81341f
Baptiste <baptiste.tab4@gmail.com>
2025-11-22 16:20:18 +0100

    UPD - Update translations to English and add support for English locale

--- lib/i18n/routing.ts
+++ lib/i18n/routing.ts
@@ -1,7 +1,7 @@
 import { defineRouting } from 'next-intl/routing'
 
 export const routing = defineRouting({
-  locales: ['fr'],
-  defaultLocale: 'fr',
+  locales: ['fr', 'en'],
+  defaultLocale: 'en',
   localePrefix: 'always',
 })
